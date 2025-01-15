e2f1cd8f0b26ac9006ba647d97338b17870b5232
DaniloPortelli <daniloportelli@gmail.com>
2025-01-15 15:15:42 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,117 @@
+// Palidroma Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+// Consigli del giorno
+// Scriviamo sempre in italiano i passaggi che vogliamo fare
+// Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+// Domande da farsi quando si crea una funzione:
+// Come dovrebbe chiamarsi?
+// Ho bisogno di parametri?
+// Devo restituire un valore?
+// Se sì, di che tipo?
+
+
+// 1. creare variabile prompt con parola
+// 2. creare funzione con charAt
+// 3. creare condizione per verificare se la parola dell'utente è uguale al risultato della funzione
+
+
+
+// let parolaUtente = "itopinonavevanonipoti"
+// const parolaUtenteSplit = parolaUtente.split("")
+//     console.log(parolaUtenteSplit)
+
+// function reverseWord (parolaUtente){
+
+//     for( let i = 0; i <= parolaUtenteSplit.length - 1; i++){
+//         let parolaUtenteSplit.charAt(i)
+//         console.log(parolaUtenteSplit.charAt(i))
+
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
+// Sommiamo i due numeri.
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
+// Dichiariamo chi ha vinto.
+
+
+
+
+
+
+let randomNumberUser = 5
+    console.log(randomNumberUser)
+
+function numberCpu (min,  max){
+    return Math.floor(Math.random() * max) + min;
+}
+numberCpu(1,5)
+const randomNumberCpu = numberCpu(1,5)
+    console.log(randomNumberCpu)
+
+
+function sommaRisultati(num1, num2) {
+    console.log(num1 + num2);
+    
+}
+    sommaRisultati( randomNumberUser, randomNumberCpu )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let randomNumberUser = 5
+//     console.log(randomNumberUser)
+
+// function numberCpu (min,  max){
+//     return Math.floor(Math.random() * max) + min;
+// }
+
+// const randomNumberCpu = numberCpu(1,5)
+//     console.log(randomNumberCpu)
+
+
+// function sommaRisultati(num1, num2) {
+//     console.log(num1 + num2);
+    
+// }
+//     sommaRisultati( randomNumberUser, randomNumberCpu )
+    
